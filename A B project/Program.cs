15c5864864e5dad8f27d3c9fff710d60927b4216
wenjen10 <wenjen10@yahoo.com>
2019-05-15 11:52:46 -0400

    How are you? added

--- A B project/Program.cs
+++ A B project/Program.cs
@@ -8,6 +8,14 @@ namespace A_B_project
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello again!");
+
+            Console.WriteLine("how are you?");
+            string answer = Console.ReadLine();
+
+           
+
+            
+
         }
     }
 }
